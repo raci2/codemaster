13435848c88753c2ede6140be0fac683ad5e4ce6
Alexandru Racicovschi <racicovschialexandru@gmail.com>
2025-05-21 20:55:32 +0100

    aula_103

---
+++ 05_php/aula_103/exercicio_01/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercício 102.3</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <h1>Inputs</h1>
+
+  <form class="caixa" action="saida.php">
+
+    <input type="text" placeholder="Nome" name="nome" required>
+    <br><br>
+    <label for="">Cor de Fundo:</label>
+    <input type="color" placeholder="cor" name="cor">
+    <br><br>
+    <input type="submit" value="Enviar">
+
+  </form>
+
+</body>
+
+</html>
---
+++ 05_php/aula_103/exercicio_01/saida.php
@@ -0,0 +1,31 @@
+<?php
+$nome = "";
+
+if(!empty($_GET["nome"])){
+  $nome = $_GET["nome"];
+}
+$cor = $_GET["cor"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercicio 102.3</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body style="background-color: <?= $cor ?>;">
+  <h1>Inputs</h1>
+
+  <div class="caixa">
+    <h3>Olá ( <?php echo $nome; ?> ), vejo qu eescolheste a cor ( <?php echo $cor; ?> )</h3>
+  </div>
+
+  <a href="index.php">
+    <button>Voltar</button>  
+  </a>
+
+</body>
+</html>
